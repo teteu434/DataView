4be9d3b2f6f151f5e52a9a806c5d9f9a76684a4d
matheus <matheushcosta434@gmail.com>
2025-02-25 09:39:14 -0300

    teste render - 25/02

--- src/backend/app/config/date.js
+++ src/backend/app/config/date.js
@@ -1,5 +1,6 @@
 export function dataTimerExpirar(){
     const timer = new Date();
-    const horas = timer.getHours() + 4;
+    var horas = timer.getHours() + 4;
+    if(horas > 24) horas = timer.getHours() + 4 - 24;
     return `${timer.getFullYear()}-${String(timer.getMonth() + 1).padStart(2, '0')}-${String(timer.getDate()).padStart(2, '0')} ${String(horas).padStart(2, '0')}:${String(timer.getMinutes()).padStart(2, '0')}:${String(timer.getSeconds()).padStart(2, '0')}`
 }
